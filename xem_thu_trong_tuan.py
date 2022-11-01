5becd85ee9bb887c1443383fe6ed69ec67fdcc14
Anh Tuấn <anhtuan@AT-iMac.local>
2022-11-02 00:23:34 +0900

    kiem tra thu trong tuan

---
+++ xem_thu_trong_tuan.py
@@ -0,0 +1,19 @@
+# kiem tra cac thu trong tuan
+thu = int(input("nhap vao mot thu trong tuan: "))
+if (thu==2):
+    print("ban nhap vao thu 2")
+elif (thu==3):
+    print("ban nhap vao thu 3")
+elif (thu==4):
+    print("ban nhap vao thu 4")
+elif (thu==5):
+    print("ban nhap vao thu 5")
+elif (thu==6):
+    print("ban nhap vao thu 6")
+elif (thu==7):
+    print("ban nhap vao thu 7")
+elif (thu==8):
+    print("ban nhap vao chu nhat")
+else:
+    print("ban nhap vao khong phai thu trong tuan")
+    print("ket thuc chuong trinh")
